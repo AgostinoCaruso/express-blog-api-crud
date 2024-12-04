491daa9f265271c31f0e818f6aa4794435ebb4ab
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-12-04 16:33:26 +0100

    add file

---
+++ router/posts.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const router = express.Router();
+
+const { lista } = require("../data/lista.js");
+
+// index
+router.get("/", function (req, res) {
+  let response = {
+    succes: true,
+    message: "Lista dei posts",
+    counter: lista.length,
+    data: [...lista],
+  };
+
+  
+
+  res.json(response);
+});
+
+// show
+router.get("/:id", function (req, res) {
+  const index = parseInt(req.params.id);
+
+  const objPost = lista.find((ele) => ele.id === index);
+  let objToShow = {};
+  if (objPost) {
+    objToShow = {
+      success: true,
+      message: "Dettaglio del post " + req.params.id,
+      objPost,
+    };
+  } else {
+    res.status(404).send("Post per indice non trovato.");
+  }
+  res.json(objToShow);
+});
+
+// store
+router.post("/", function (req, res) {
+  res.send("Creazione nuova pizza");
+});
+
+// update
+router.put("/:id", function (req, res) {
+  res.send("Modifica integrale della pizza " + req.params.id);
+});
+
+// modify
+router.patch("/:id", function (req, res) {
+  res.send("Modifica parziale della pizza " + req.params.id);
+});
+
+// destroy
+router.delete("/:id", function (req, res) {
+  res.send("Eliminazione della pizza " + req.params.id);
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,42 @@
+/**
+ * Creiamo il nostro blog personale e giorno dopo giorno lo potremo arricchire con nuove funzionalità sulla base di quello che impareremo.
+- Creiamo il progetto base con una rotta / che ritorna un testo semplice con scritto ”Server del mio blog”
+- Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe)
+- Creiamo poi una rotta /bacheca che restituisca un oggetto json con la lista dei post e il conteggio, partendo da un array.
+- Configuriamo gli asset statici sull’applicazione in modo che si possano visualizzare le immagini associate ad ogni post.
+- Testare su postman
+Bonus:
+filtrare i dati sulla base di parametri in query string
+ */
+/**
+ *
+ */
+//server
+const express = require("express"); //associare a variabile l'oggetto express
+const server = express();
+//const PORT = 3000;
+const port = process.env.PORT;
+//other var
+server.use(express.static("public"));
+//root
+const postRouter = require("./router/posts.js");
+server.use("/post", postRouter);
+
+//qua partendo dal port 3000 ho l'inizio del server
+server.get("/", (req, res) => {
+  res.send("<h1>Server del mio blog</h1>");
+});
+
+
+
+
+//default catch for typo url
+server.get("*", (req, res) => {
+  res.status(404).send("<h1>ERROR - 404 page not found</h1>");
+});
+
+//qua sto in ascolto per la porta 3000
+server.listen(port, () => {
+  console.log("Server del mio blog");
+  console.log(`Server running on http://localhost:${port}`);
+});
